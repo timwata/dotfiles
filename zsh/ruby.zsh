d9e2bdd46ee7f8a0da83dae870409d524db9a0e0
timwata <webmaster@watatim.com>
2014-03-31 22:13:58 +0900

    fix typo

--- zsh/ruby.zsh
+++ zsh/ruby.zsh
@@ -4,5 +4,5 @@ then
   git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 fi
 
-export PATH="${HOME}/.rbenv/bin:/${HOME}/.rbenv/shims:${PATH}"
+export PATH="${HOME}/.rbenv/bin:${HOME}/.rbenv/shims:${PATH}"
 eval "$(rbenv init -)"
