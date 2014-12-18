2164642ff753f5608ab68526947c89b406291aa2
timwata <webmaster@watatim.com>
2014-12-18 05:20:42 +0000

    bugfix

--- zsh/ruby.zsh
+++ zsh/ruby.zsh
@@ -1,4 +1,4 @@
-if [[ ! -d ${HOME}/.rbenv ]]
+if [ ! -f ${HOME}/.rbenv/bin/rbenv ] || [ ! -d ${HOME}/.rbenv/plugins/ruby-build ]
 then
     git clone https://github.com/sstephenson/rbenv.git      ~/.rbenv
     git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
